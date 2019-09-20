e18c50afb1670e53fcf1484f1bbfcaf563ee43c1
CooperR97 <32103346+CooperR97@users.noreply.github.com>
2019-09-20 16:22:33 -0500

    Add files via upload

---
+++ Get-ProcessOwner/Get-ProcessOwner1.0.ps1
@@ -0,0 +1,9 @@
+﻿#Cooper Redfern
+#09/11/2019
+#Return a list of win32_process objects where the process is Microsoft Project
+$process = 'someProcess'
+$Users = (Get-WmiObject Win32_Process -ComputerName 'someDomain' | ?{ $_.ProcessName -match $process }).GetOwner() 
+#Print User attribute of each object returned
+foreach($User in $Users){
+write-host($User.User)
+}
